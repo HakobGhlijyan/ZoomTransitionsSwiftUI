6bdec631d333a8a85339df3a6f0e05c7995e666e
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2025-01-18 23:52:38 +0400

    Initial Commit

---
+++ ZoomTransitionsSwiftUI/ZoomTransitionsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomTransitionsSwiftUIApp.swift
+//  ZoomTransitionsSwiftUI
+//
+//  Created by Hakob Ghlijyan on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ZoomTransitionsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
